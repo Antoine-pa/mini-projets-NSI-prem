f14b157c93dbc81eaad6752671da7ded4acc7a72
Antoine-pa <68688365+Antoine-pa@users.noreply.github.com>
2022-03-24 09:53:11 +0100

    Update interface.py

--- jeu_de_la_vie/interface.py
+++ jeu_de_la_vie/interface.py
@@ -4,9 +4,6 @@ import time
 
 pygame.init()
 
-SPEED = 1
-FONT = pygame.font.Font("Melon Honey.ttf", 40)
-
 #Couleurs
 BLACK = (0, 0, 0)
 WHITE = (255, 230, 240)
@@ -14,22 +11,42 @@ GREY_WHITE = (200, 200, 200)
 GREY = (100, 100, 100)
 dic_grid_color = {0 : GREY, 1 : GREY_WHITE}
 
+#MENU
+SIZE_TEXT = 20
+active = None
+#input box x
+text_input_box_x = "15"
+
+RECT_INPUT_BOX_X = (100, 100, 140, 32)
+RECT_CLEAR_INPUT_BOX_X = (RECT_INPUT_BOX_X[0]+3, RECT_INPUT_BOX_X[1]+3, RECT_INPUT_BOX_X[2]-6, RECT_INPUT_BOX_X[3]-6)
+POS_TEXT_BOX_X = (RECT_INPUT_BOX_X[0] + 5, RECT_INPUT_BOX_X[1]+9)
+THICKNESS_INPUT_BOX_X = 2
+#input box y
+text_input_box_y = "10"
+
+RECT_INPUT_BOX_Y = (100, 150, 140, 32)
+RECT_CLEAR_INPUT_BOX_Y = (RECT_INPUT_BOX_Y[0]+3, RECT_INPUT_BOX_Y[1]+3, RECT_INPUT_BOX_Y[2]-6, RECT_INPUT_BOX_Y[3]-6)
+POS_TEXT_BOX_Y = (RECT_INPUT_BOX_Y[0] + 5, RECT_INPUT_BOX_Y[1]+9)
+THICKNESS_INPUT_BOX_Y = 2
+
 #initialisation de la grille
-grid = new_grid(15, 10)
+grid = new_grid(int(text_input_box_x), int(text_input_box_y))
+SPEED = 1
 
 #Dimensions
 screen_info = pygame.display.Info()
 THICKNESS = 3
 
-WINDOW_Y = screen_info.current_h
-NUMBER_LINE = len(grid)
-SPACE_Y = WINDOW_Y//NUMBER_LINE - THICKNESS
+SIZE_MENU_X = screen_info.current_w // 5
 
-WINDOW_X = screen_info.current_w
-SIZE_MENU_X = WINDOW_X // 5
-WINDOW_X -= SIZE_MENU_X
+WINDOW_X = screen_info.current_w - SIZE_MENU_X
+WINDOW_Y = screen_info.current_h
+
 NUMBER_COLUMN = len(grid[0])
+NUMBER_LINE = len(grid)
+
 SPACE_X = WINDOW_X//NUMBER_COLUMN - THICKNESS
+SPACE_Y = WINDOW_Y//NUMBER_LINE - THICKNESS
 
 #Initialisation de la fenêtre
 screen = pygame.display.set_mode((SIZE_MENU_X + WINDOW_X, WINDOW_Y))
@@ -43,35 +60,52 @@ def refresh(grid):
     for y in range(NUMBER_LINE):
         for x in range(NUMBER_COLUMN):
             pygame.draw.rect(screen, dic_grid_color[get_element(x, y, grid)], (SIZE_MENU_X+x*(SPACE_X+THICKNESS), y*(SPACE_Y+THICKNESS), SPACE_X, SPACE_Y))
-
     pygame.display.update()
+
+def update_settings_grid(x, y):
+    grid = new_grid(x, y)
+    NUMBER_COLUMN = len(grid[0])
+    NUMBER_LINE = len(grid)
+
+    SPACE_X = WINDOW_X//NUMBER_COLUMN - THICKNESS
+    SPACE_Y = WINDOW_Y//NUMBER_LINE - THICKNESS
+
+    pygame.draw.rect(screen, BLACK, (SIZE_MENU_X, 0, SIZE_MENU_X + WINDOW_X, WINDOW_Y))
+
+    return grid, NUMBER_COLUMN, NUMBER_LINE, SPACE_X, SPACE_Y
+
+
+
 
 refresh(grid)
 
-def Text(text, color, pos):
+def Text(text, color, pos, size):
+    FONT = pygame.font.Font("Melon Honey.ttf", size)
     screen.blit(FONT.render(text, True, color), pos)
+    pygame.display.update()
+    del FONT
 
 
 
-#menu
-Text("le jeu de la vie", BLACK, (40, 30))
-active = None
-input_box_x = pygame.Rect(100, 100, 140, 32)
-text_input_box_x = "15"
-pygame.draw.rect(screen, BLACK, input_box, 2)
-pygame.display.update()
+#init menu
+Text("le jeu de la vie", BLACK, (40, 30), 40)
+input_box_x = pygame.Rect(*RECT_INPUT_BOX_X)
+pygame.draw.rect(screen, BLACK, input_box_x, THICKNESS_INPUT_BOX_X)
+Text(text_input_box_x, BLACK, POS_TEXT_BOX_X, SIZE_TEXT)
+
+input_box_y = pygame.Rect(*RECT_INPUT_BOX_Y)
+pygame.draw.rect(screen, BLACK, input_box_y, THICKNESS_INPUT_BOX_Y)
+Text(text_input_box_y, BLACK, POS_TEXT_BOX_Y, SIZE_TEXT)
 
 
 #Initialisation des variables
 continuer = True
 start_generation = False
 start_at = time.time()
-
 #Boucle infinie
 while continuer:
     #Evénements
     for event in pygame.event.get():
-
         #Quitter
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -92,34 +126,57 @@ while continuer:
 
         #Mettre en pause
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE: #lancer le jeu
                 start_generation = not start_generation
 
-            if event.key == pygame.K_ESCAPE:
-                del FONT
+            if event.key == pygame.K_ESCAPE: #quitter
                 pygame.display.quit()
                 continuer = False
 
-            if active == "input_box_x":
-                if event.key == pygame.K_BACKSPACE:
+            if active == "input_box_x": #si il écrit dans la zone de texte
+                if event.key == pygame.K_BACKSPACE: #si il supp
                     text_input_box_x = text_input_box_x[:-1]
+
                 elif event.key == pygame.K_RETURN:
-                    pass
+                    grid, NUMBER_COLUMN, NUMBER_LINE, SPACE_X, SPACE_Y = update_settings_grid(int(text_input_box_x), int(text_input_box_y))
+                    refresh(grid)
                 else:
                     el = event.unicode
-                    print(el)
                     if el.isdigit():
                         text_input_box_x += el
-                        print(text_input_box_x)
+
+                if event.key != pygame.K_RETURN:
+                    pygame.draw.rect(screen, WHITE, pygame.Rect(*RECT_CLEAR_INPUT_BOX_X), 0)
+                    Text(text_input_box_x, BLACK, POS_TEXT_BOX_X, SIZE_TEXT)
+                    pygame.display.update()
+
+            elif active == "input_box_y": #si il écrit dans la zone de texte
+                if event.key == pygame.K_BACKSPACE: #si il supp
+                    text_input_box_y = text_input_box_y[:-1]
+
+                elif event.key == pygame.K_RETURN:
+                    grid, NUMBER_COLUMN, NUMBER_LINE, SPACE_X, SPACE_Y = update_settings_grid(int(text_input_box_x), int(text_input_box_y))
+                    refresh(grid)
+                else:
+                    el = event.unicode
+                    if el.isdigit():
+                        text_input_box_y += el
+
+                if event.key != pygame.K_RETURN:
+                    pygame.draw.rect(screen, WHITE, pygame.Rect(*RECT_CLEAR_INPUT_BOX_Y), 0)
+                    Text(text_input_box_y, BLACK, POS_TEXT_BOX_Y, SIZE_TEXT)
+                    pygame.display.update()
+
 
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            # If the user clicked on the input_box_x rect.
             if input_box_x.collidepoint(event.pos):
-                # Toggle the active variable.
                 active = "input_box_x"
+            elif input_box_y.collidepoint(event.pos):
+                active = "input_box_y"
             else:
                 active = None
+
 
 
     #Le jeu se joue
@@ -129,6 +186,7 @@ while continuer:
             start_at = time.time()
             grid = gen(grid)
             refresh(grid)
+            pygame.display.update()
 
 
 pygame.quit()
