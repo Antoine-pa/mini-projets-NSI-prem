e1f2b728b7f1ba2f5eacd266f89f087f2f95e6f9
Antoine-pa <68688365+Antoine-pa@users.noreply.github.com>
2022-04-07 09:04:46 +0200

    Update core.py

--- jeu_de_la_vie/core.py
+++ jeu_de_la_vie/core.py
@@ -8,6 +8,7 @@ def new_grid(len_x, len_y): #pour créer une nouvelle grille
 
 
 def get_element(x, y, grid): #pour récupérer un élément
+
     return grid[y][x]
 
 def set_element(x, y, value, grid): #pour enregister un élément
@@ -31,7 +32,7 @@ def gen(old_grid): #pour faire une nouvelle génération de cellule
             nb_in_alive = 0 #nombre de cellules voisines en vie
             for y2 in range(y-1, y+2): #on parcours les voisines
                 for x2 in range(x-1, x+2): #on parcours les voisines
-                    if x2 >= 0 and x2 < 10 and y2 >= 0 and y2 < 10:
+                    if x2 >= 0 and x2 < len(old_grid[0]) and y2 >= 0 and y2 < len(old_grid):
                         nb_in_alive += get_element(x2, y2, old_grid)
             if el == 1: #si la cellule actuelle est en vie
                 nb_in_alive -= 1 #on la remet à 0
@@ -40,20 +41,3 @@ def gen(old_grid): #pour faire une nouvelle génération de cellule
                 grid = set_element(x, y, int(nb_in_alive == 3), grid) #on change son état
 
     return grid
-
-"""
-grid = new_grid()
-
-for i in range(3):
-    grid = click((2, i+1), grid)
-
-nb_gen = 3
-for generation in range(nb_gen):
-    print(f"========== gen {generation + 1}")
-    for line in grid:
-        print(line)
-    grid = gen(grid)
-print(f"========== gen {nb_gen + 1}")
-for line in grid:
-    print(line)
-"""
